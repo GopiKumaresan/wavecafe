6afd5cb5361a51b4cc2b52c5b4958d1e44e8f7d9
GopiKumaresan <59858691+GopiKumaresan@users.noreply.github.com>
2020-11-06 10:07:50 +0530

    Update order.php

--- order.php
+++ order.php
@@ -1,7 +1,7 @@
 <html>
 <body>
     <?php
-    $db = pg_connect('host=localhost dbname=postgres user=myusername password=mypassword');
+    $db = pg_connect('host=ec2-3-208-224-152.compute-1.amazonaws.com dbname=ddu9arc8uui5av user=zmijixqhillrkw password=a083f7c8337901129bc672b10311667e458ade626d089ffa30f0962c71f67c65');
 
     $custname = pg_escape_string($_POST['name']);
     $custorder = pg_escape_string($_POST['order']);
